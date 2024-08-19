6afa9420bd18158088d3706c76ce0ec59e72eb26
Helene <40670839+Bertiscoding@users.noreply.github.com>
2024-08-19 14:36:59 +0100

    send api requests query

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import React, {FC, ReactElement} from "react"
-import { ThemeProvider, CssBaseline } from '@mui/material'
-import darkTheme from './theme/darkTheme'
-import Dashboard from "./pages/dashboard/dashboard"
+import React, {FC, ReactElement} from "react";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import darkTheme from "./theme/darkTheme";
+import Dashboard from "./pages/dashboard/dashboard";
+
+// TanStack:
+const queryClient = new QueryClient(); 
 
 const App: FC = (): ReactElement => {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <Dashboard />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Dashboard />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 export default App
