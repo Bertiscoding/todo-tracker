0758327140d1a3d6a99f95b2dcfa9a73cd76c4a7
Helene <40670839+Bertiscoding@users.noreply.github.com>
2024-08-21 13:54:19 +0100

    init context api

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,20 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tseslint from "@typescript-eslint/eslint-plugin";
 import pluginReact from "eslint-plugin-react";
-
+import parser from "@typescript-eslint/parser";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      parser: parser,
+      globals: globals.browser,
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import darkTheme from "./theme/darkTheme";
 import Dashboard from "./pages/dashboard/dashboard";
+import ComposeContext from "./context/Compose.context";
+import { rootContext } from "./context/root.context";
 
 // TanStack:
 const queryClient = new QueryClient(); 
@@ -11,12 +13,14 @@ const queryClient = new QueryClient();
 const App: FC = (): ReactElement => {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        <Dashboard />
-      </ThemeProvider>
+      <ComposeContext components={rootContext}>
+        <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
+          <Dashboard />
+        </ThemeProvider>
+      </ComposeContext>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
 }
-export default App
+export default App;
